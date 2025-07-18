5d776d81a326e062e27f622a5d74d9f21cec94a0
킨 <69646118+Roflane@users.noreply.github.com>
2025-07-18 11:34:55 +0400

    update

--- Project/Subqueries.sql
+++ Project/Subqueries.sql
@@ -1,3 +1,4 @@
+-- Students
 select s.Name, s.Surname, sum(p.Amount) as TotalPenalty
 from Students as s
 join Penalties as p on s.Id = p.StudentId
@@ -6,26 +7,57 @@ having sum(p.Amount) > (
     select avg(Amount) from Penalties
 );
 
+-- Teachers
 select Name, Surname, Salary
 from Teachers
 where Salary > (
     select avg(Salary) from Teachers
 );
 
+-- Students
 select Name, Surname
 from Students
 where Id in (
     select top 5 StudentId from Leaderboard order by Place
 );
 
+-- Deans
 select Name, Surname
 from Deans
 where Salary in (
     select Salary from Deans where Salary > 10000
 );
 
+-- Staff
 select Name, Surname
 from Staff
 where Salary in (
-    Select Salary from Staff where Salary between 10000 and 35000
+    select Salary from Staff where Salary between 10000 and 35000
 );
+
+-- Faculties
+select Name
+from Faculties
+where Name in (
+    select Name where Name like 'E%'
+);
+
+select Name
+from Faculties
+where Name in (
+    select Name where Name like 'M%'
+);
+
+-- Groups
+select RoomNumber
+from Groups
+where Name in (
+    select Name where Name like 'CS-%'
+);
+
+-- Departments
+select Financing
+from Departments
+where Financing in (
+    select Financing where Financing > 500000
+)
--- Project/Views.sql
+++ Project/Views.sql
@@ -1,7 +1,10 @@
 DROP VIEW IF EXISTS v_StudentPenalties;
 DROP VIEW IF EXISTS v_TeacherIncome;
 DROP VIEW IF EXISTS v_LeaderboardTop5;
+DROP VIEW IF EXISTS v_StaffTop5;
+DROP VIEW IF EXISTS v_UniversityTop5;
 
+-- Students
 create view v_StudentPenalties as
 select
     s.Id,
@@ -12,6 +15,7 @@ from Students s
 left join Penalties p on s.Id = p.StudentId
 group by s.Id, s.Name, s.Surname, p.Amount;
 
+-- Teachers
 create view v_TeacherIncome as
 select
     Id,
@@ -22,6 +26,7 @@ select
     (Salary + Premium) as TotalIncome
 from Teachers;
 
+-- Leaderboard
 create view v_LeaderboardTop5 as
 select
     s.Name,
@@ -31,6 +36,7 @@ from Leaderboard l
 join Students as s on l.StudentId = s.Id
 where l.Place <= 5;
 
+-- Staff
 create view v_StaffTop5 as
 select
     Name,
@@ -38,6 +44,7 @@ select
 from Staff
 where Salary > 30000;
 
+-- University
 create view v_UniversityTop5 as
 select
     Id,
